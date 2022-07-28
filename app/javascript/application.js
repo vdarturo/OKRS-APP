fce318afd6ca00001d3badda683a7a16aa94173e
Arturo Velazquez <arturovelazquez@Air-de-Arturo.huawei.net>
2022-07-27 22:54:11 -0500

    Configure layout bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery
+//= require popper
+//= require bootstrap
+//= require rails-ujs
+//= require sb-admin-2.min
+//= require activestorage
+//= require turbolinks
+//= require_tree .
